528ea7390b6b326cdf2bf5af5feefbd5719bee83
ps26102010 <163730755+ps26102010@users.noreply.github.com>
2024-06-14 12:53:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const correctPassword = 'nigger';  // Set the password to "password"
+
+document.getElementById('submitPasswordButton').addEventListener('click', function() {
+    const enteredPassword = document.getElementById('passwordInput').value;
+    if (enteredPassword === correctPassword) {
+        document.getElementById('downloadSection').style.display = 'block';
+        document.getElementById('passwordInput').style.display = 'none';
+        document.getElementById('submitPasswordButton').style.display = 'none';
+        document.getElementById('passwordHint').style.display = 'none';
+    } else {
+        alert('Incorrect password. Please try again.');
+    }
+});
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    var link = document.createElement('a');
+    link.href = 'New Folder/cysx.docx';  // Update with your document's path
+    link.download = 'cysx.docx';  // Update with your document's name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
